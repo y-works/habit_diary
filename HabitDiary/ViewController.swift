6cddfad9186b3f5f0b116bc13ea4b694ed9b3500
Hiroyuki Narita <davidoff1125@gmail.com>
2017-08-22 20:20:34 +0900

    Initial Commit

---
+++ HabitDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HabitDiary
+//
+//  Created by 成田裕之 on 2017/08/22.
+//  Copyright © 2017年 Hiroyuki Narita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
